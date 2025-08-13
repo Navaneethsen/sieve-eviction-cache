1e18a0487b8da134f8f74046c6283d8b1c4acfa6
Navaneeth Sen <navaneethsen@gmail.com>
2025-08-13 23:26:22 +0200

    Update IStatisticalCache.java

--- src/main/java/org/cache/common/IStatisticalCache.java
+++ src/main/java/org/cache/common/IStatisticalCache.java
@@ -1,16 +1,25 @@
 /*
+ * MIT License
  *
- * Copyright (c) 2025  Pegasystems Inc.
- * All rights reserved.
+ * Copyright (c) 2025 Navaneeth Sen <navaneethsen@gmail.com>
  *
- * This  software  has  been  provided pursuant  to  a  License
- * Agreement  containing  restrictions on  its  use.   The  software
- * contains  valuable  trade secrets and proprietary information  of
- * Pegasystems Inc and is protected by  federal   copyright law.  It
- * may  not be copied,  modified,  translated or distributed in  any
- * form or medium,  disclosed to third parties or used in any manner
- * not provided for in  said  License Agreement except with  written
- * authorization from Pegasystems Inc.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package org.cache.common;
 
